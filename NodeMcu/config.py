bc8da319b2c8eec20034fdd1e2430a8116d306b5
Jush <aaiiaann2000@gmail.com>
2019-12-18 11:45:16 +0800

    新增NodeMcu 串接 Micro Servo 端口

---
+++ NodeMcu/config.py
@@ -0,0 +1,15 @@
+def do_connect():
+    SSID = 'Jiahsin iPhone'
+    PASSWORD = 'hellojiahsin'
+
+    sta_if = network.WLAN(network.STA_IF)
+    ap_if = network.WLAN(network.AP_IF)
+    if ap_if.active():
+        ap_if.active(False)
+    if not sta_if.isconnected():
+        print('connecting to network...')
+        sta_if.active(True)
+        sta_if.connect(SSID, PASSWORD)
+        while not sta_if.isconnected():
+            pass
+    print('Network configuration:', sta_if.ifconfig())
---
+++ NodeMcu/main.py
@@ -0,0 +1,34 @@
+from machine import PWM, Pin
+import time
+import network
+
+servo = PWM(Pin(4), freq=50)
+
+def do_connect():
+    SSID = 'Jiahsin iPhone'
+    PASSWORD = 'hellojiahsin'
+
+    sta_if = network.WLAN(network.STA_IF)
+    ap_if = network.WLAN(network.AP_IF)
+    if ap_if.active():
+        ap_if.active(False)
+    if not sta_if.isconnected():
+        print('connecting to network...')
+        sta_if.active(True)
+        sta_if.connect(SSID, PASSWORD)
+        while not sta_if.isconnected():
+            pass
+    print('Network configuration:', sta_if.ifconfig())
+
+
+def main():
+	do_connect()
+	for i in range(10):
+		servo.duty(50)
+		time.sleep(1)
+		servo.duty(100)
+		time.sleep(1)
+if __name__ == "__main__":
+    main()
+
+	
